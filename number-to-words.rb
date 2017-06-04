226613ca3e79284bdd5deb093c72e6ed2d58c27d
Andrew Gordon <agordon@videologygroup.com>
2017-06-03 22:37:04 -0400

    output 1 digit

--- number-to-words.rb
+++ number-to-words.rb
@@ -1,38 +1,19 @@
-# print "Enter a number: "
-# n = gets.chomp
-# puts "----"
-# puts "Your number has #{n.length} digits."
+print "Enter a number: "
+n = gets.chomp
+digits = n.to_s.length
+n = n.to_i
+puts "----"
+puts "Digits: #{digits}."
+puts "----"
 
 ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-puts ones[0]
-puts teens[0]
-puts tens[3] + '-' + ones[3]
-puts tens[8] + '-' + ones[9]
-
-
-# n.each_char { |c| 
-# 	if c == '1'
-# 		print 'one '
-# 	elsif c == '2'
-# 		print 'two '
-# 	elsif c == '3' 
-# 		print 'three '
-# 	elsif c == '4'
-# 		print 'four '
-# 	elsif c == '5'
-# 		print 'five '
-# 	elsif c == '6'
-# 		print 'six '
-# 	elsif c == '7'
-# 		print 'seven '
-# 	elsif c == '8'
-# 		print 'eight '
-# 	elsif c == '9'
-# 		print 'nine '
-# 	elsif c == '0'
-# 		print 'zero '	
-# 	end
-# }
+if digits == 0 
+	puts "Bummer - you didn't enter anything."
+elsif digits == 1 
+	puts ones[n]
+ else
+	puts "I don't know how to process."
+end
