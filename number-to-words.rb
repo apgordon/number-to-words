4f687044f9527cf1dbfed7e4da4cbcfd3c1ceb0c
Andrew Gordon <agordon@videologygroup.com>
2017-06-03 21:56:29 -0400

    output individual numbers as words

--- number-to-words.rb
+++ number-to-words.rb
@@ -1,1 +1,27 @@
-puts "Hello, World"
+print "Enter a number: "
+n = gets.chomp
+puts "----"
+puts "Your number has #{n.length} digits."
+n.each_char { |c| 
+	if c == '1'
+		print 'one '
+	elsif c == '2'
+		print 'two '
+	elsif c == '3' 
+		print 'three '
+	elsif c == '4'
+		print 'four '
+	elsif c == '5'
+		print 'five '
+	elsif c == '6'
+		print 'six '
+	elsif c == '7'
+		print 'seven '
+	elsif c == '8'
+		print 'eight '
+	elsif c == '9'
+		print 'nine '
+	elsif c == '0'
+		print 'zero '	
+	end
+}
