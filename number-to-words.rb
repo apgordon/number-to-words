8782bc2670b5df9d8b6bb748a27764aeba9d8ae9
Andrew Gordon <agordon@videologygroup.com>
2017-06-03 23:16:20 -0400

    up to 99

--- number-to-words.rb
+++ number-to-words.rb
@@ -1,24 +1,29 @@
 print "Enter a number: "
 n = gets.chomp
-digits = n.to_s.length
+digits_count = n.to_s.length
 n = n.to_i
 puts "----"
-puts "Digits: #{digits}."
+puts "digits_count: #{digits_count}."
 puts "----"
 
 ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-if digits == 0 
+digits = n.to_s.split("")
+
+if digits_count == 0 
 	puts "Bummer - you didn't enter anything."
-elsif digits==1 
+elsif digits_count == 1 
 	puts ones[n]
-elsif digits==2 && n!=20
-	x = n.to_s.split("")
-	puts teens[x.last.to_i]
-elsif digits==2 && n==20
+elsif digits_count == 2 && n < 20
+	puts teens[digits.last.to_i]
+elsif digits_count == 2 && n == 20
 	puts tens[2]
- else
+elsif digits_count == 2 && n > 20 && digits.last.to_i != 0
+	puts tens[digits.first.to_i] + '-' + ones[digits.last.to_i]
+elsif digits_count == 2 && n > 20 && digits.last.to_i == 0
+		puts tens[digits.first.to_i]
+else
 	puts "I don't know how to process."
 end
